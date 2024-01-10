7c94ca750e9a4adc2bb4c2c9ed070406da29f253
WesleyMaik <wesleynamikaze404@gmail.com>
2024-01-09 22:00:09 -0400

    feat: add getProducts method

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,9 +1,29 @@
-import { Product } from "./Product";
+import { Product } from "./types/Product";
 
 const serverUrl = "http://localhost:5000";
+const get_product_url = `${serverUrl}/products`;
 
-function main() {
-  console.log(serverUrl);
+async function getProducts() {
+  let data: Product | null = null;
+  let error: string | null = null;
+
+  try {
+    const response = await fetch(get_product_url, { method: "GET" });
+
+    if (!response.ok) {
+      throw new Error("Something went wrong.");
+    }
+
+    const result = (await response.json()) as Product;
+
+    data = result;
+  } catch (err) {
+    error = err;
+  }
+
+  return { data, error };
 }
+
+async function main() {}
 
 document.addEventListener("DOMContentLoaded", main);
